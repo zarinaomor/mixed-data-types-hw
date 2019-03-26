94d498ffd84a5c101e74e4f002de16ff25831f4c
zarinaomor <zarinaomorova@yahoo.com>
2019-03-25 21:16:44 -0700

    data structures

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
-console.log('test');
+// A light switch that can be either on or off.
+// Boolean
+
+// A user's email address.
+// 'string'
+
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// Array
+
+// A list of student names from our class.
+// object
+
+// A list of student names from our class, each with a location.
+// Object
+
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// Object that contains Array
+
+
+
+// Make an array that holds all of the colors of the rainbow.
+const array = ['green', 'yellow', 'blue', 'red', 'orange', 'indigo', 'violet']
+
+// Write code that will access "blue" from the rainbow array.
+console.log(array[2]);
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+const me = {
+    name: 'Zarina',
+    favorite_food: 'Pasta',
+    hobby: 'tennis',
+    location: 'LA',
+    favorite_dataType: true
+}
+
+
+// Write code that will access your hobby from the object that you just created.
+
+console.log(me.hobby);
+
+
+
+
